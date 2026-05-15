26c7a4a22ed57c0681c2e24dd704478b8cdfaf00
svernat <stephane.vernat@gmail.com>
2026-05-16 01:26:52 +0200

    Répartition des exercices en répertoires

---
+++ exercice-1/src/main/java/com/sve/workshops/hexagonal/hexagonal/HexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.sve.workshops.hexagonal.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HexagonalApplication.class, args);
+	}
+
+}
